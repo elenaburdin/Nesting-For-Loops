5657de6748d0ab74bc63c4865119d2edc3b72e7b
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2023-02-04 20:31:29 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            product = product * arr[i][j];
+        }
+    }
+
+    return product;
+}
